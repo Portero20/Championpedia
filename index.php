9d065de61c876472b31885bb83a12f9903ba1cdb
LuchitoSab <93064995+LuchitoSab@users.noreply.github.com>
2023-01-27 20:08:11 -0300

    primer commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+    <a href="">Jugadores</a>
+    <br>
+    <br>
+    <a href="">Equipos</a>
+    <br>
+    <br>
+<form action="node.js" method="post >
+        <br>
+        <br>
+        <label for="">Elegir Categiría</label>
+        <select name="wiki" id=""> 
+            <option value="">jugadores</option>
+            <option value="dog">equipos</option>
+        </select>
+        <br>
+        <br>
+        <label for="">titulo</label>
+        <input type="text">
+        <br>
+        <br>
+        <label for="">autor</label>
+        <input type="text">
+        <br>
+        <br>
+        <label for="">texto</label>
+        <input type="text">
+        <br>
+        <br>
+        <input type="file">
+        <br>
+        <br>
+        <input type="submit">
+    </form>
+    <script src="/node.js"></script>
+    
+</body>
+</html>
