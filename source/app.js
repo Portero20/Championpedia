55a084744597be89f52953dc0b3d1ae0abf20dc3
Julián Viera <julianviera2019@hotmail.com>
2023-02-01 00:23:16 -0300

    Julián - instalación de cors

--- source/app.js
+++ source/app.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors')
 const { port, start } = require('./modules/port')
 
+// Permitir recibir solicitudes de otros dominios
+app.use(cors())
+
 // req.body
 app.use(bodyParser.json()); // para analizar aplicación/json
 app.use(bodyParser.urlencoded({ extended: false })); // para analizar la aplicación/x-www-form-urlencoded
