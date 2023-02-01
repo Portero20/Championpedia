8f83a984d474644665e991b21c287c9709a8882c
Julián Viera <julianviera2019@hotmail.com>
2023-02-01 00:22:14 -0300

    Julián - instalación de cors

--- source/app.js
+++ source/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const { port, start } = require('./modules/port')
 
 // req.body
