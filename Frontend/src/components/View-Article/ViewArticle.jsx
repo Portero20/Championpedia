7803bbd0f91039a6693ec0890d7961c21c39daf4
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-02-03 20:46:52 -0300

    Exequiel - Terminando vista de articulos

--- Frontend/src/components/View-Article/ViewArticle.jsx
+++ Frontend/src/components/View-Article/ViewArticle.jsx
@@ -1,24 +1,210 @@
+import '../../scss/utilities/_utilities.scss';
+
 import React from 'react'
+import pele from '../../img/pele.jpg';
 
 const ViewArticle = () => {
 return (
+
+
 <div>
 
-  <div className='containerPadre__View'>
+  <div className='containerView'>
 
-    <div className='containerView'>
+    <div className="descripcionView">
 
-      <div className='containerView__estilo'>
+      <div className='containerView-estilo'>
 
-        <h4>El rey Pele</h4>
+        <h3 className='containerView-titulo'>El rey Pele</h3>
+
+      </div>
+
+      <p className='parrafoView'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, dolor ipsum. In
+        excepturi aperiam qui quis aliquid magni nihil iure! Quis soluta dolore eligendi autem suscipit architecto,
+        reprehenderit doloribus a?
+        Animi iusto sunt odit excepturi rerum labore minima dolore fugit minus. Saepe mollitia ipsum iure. Nobis
+        soluta excepturi fugiat pariatur doloribus delectus aperiam, veniam, accusamus, consequatur dolorum quaerat
+        tempora error!
+        Labore, quo vel! A ipsum, praesentium ratione totam neque facilis illo. Impedit, et optio necessitatibus sint,
+        rerum, velit praesentium a voluptatibus modi delectus laboriosam ipsum similique asperiores qui ea in!
+        Earum temporibus nihil provident nisi dicta molestias doloribus vitae impedit quidem placeat itaque fugiat
+        recusandae repellendus deserunt similique nobis aut, quia, alias tenetur magni eius? Harum vel fugit cum sunt?
+        Quae necessitatibus suscipit nisi tenetur ducimus illo ea, maiores commodi doloribus totam amet labore,
+        obcaecati tempore quaerat officiis vel. Beatae molestias nesciunt at ratione dolores quidem odio sunt rem
+        natus.
+        Quae, provident praesentium architecto qui autem ratione rem cupiditate asperiores quia perspiciatis, ullam
+        dolor doloremque, voluptate atque incidunt! Alias, culpa quo? Porro libero velit nisi reiciendis temporibus
+        sequi quam itaque.
+        Quod asperiores tempora doloremque, quo reprehenderit sunt praesentium! Veniam porro, ipsam praesentium totam
+        sapiente sequi non ab recusandae earum fugit at itaque magnam numquam reprehenderit! Deserunt nesciunt debitis
+        sint? Quaerat.
+        Aliquam mollitia magnam ipsam optio quas modi adipisci. Iure voluptas possimus inventore dolore id officia
+        quos tempore qui reprehenderit cumque! Repellat laborum blanditiis voluptate aspernatur. Perspiciatis ad
+        laborum ipsa enim.
+        Maxime aperiam voluptate voluptatum magni adipisci quam dicta quidem in amet vero laboriosam dolor eveniet
+        suscipit tenetur magnam iure fugit doloribus, porro neque quis, quae quas! Iure accusantium tempora ipsa.
+        Ipsa, exercitationem? Aut cumque laboriosam a perspiciatis beatae dicta tempora natus ab eaque qui et quaerat
+        aliquid architecto ipsam facilis nemo itaque quasi eum laudantium, sequi minus officia, non corporis.
+        Mollitia quae tenetur possimus blanditiis a asperiores cumque vitae aut, ipsam soluta neque, velit illo
+        laudantium ducimus magni quas voluptatem omnis facere. Eius sint asperiores explicabo placeat, animi quia
+        repellendus.
+        Unde, error sed porro possimus praesentium, distinctio in asperiores officiis nesciunt aliquam recusandae
+        minus magni voluptate facilis. Aliquam earum dolorum sed in dolor expedita, quo a, pariatur repellat doloribus
+        cupiditate!
+        Architecto et rem odit voluptates totam, id cupiditate dolorem aut enim asperiores pariatur illum voluptatum
+        itaque nobis nisi eaque quaerat repudiandae iste inventore accusantium ratione voluptatibus. Eos itaque nihil
+        cumque?
+        Illo, eum earum, maxime praesentium, libero aperiam aut ex ut amet unde asperiores nam fugiat dolorum ullam
+        nihil corrupti? Optio, perspiciatis provident maxime laboriosam eaque illum incidunt? Fugiat, saepe
+        blanditiis.
+        Hic aliquam quas incidunt natus, unde omnis quam facere, quo similique perferendis voluptatum harum suscipit
+        iure laboriosam explicabo, perspiciatis rem. Sapiente molestiae laudantium eos placeat provident esse voluptas
+        aspernatur alias.
+        Molestiae obcaecati ipsa, assumenda inventore repellendus quia cumque ut vitae maiores labore voluptatem
+        reprehenderit iusto soluta a, fugit qui delectus commodi illum quibusdam? Maxime, dignissimos autem ex
+        veritatis rerum minima.
+        Saepe recusandae rerum veritatis quaerat accusantium debitis tenetur ipsam possimus? Dignissimos nesciunt
+        dolorum recusandae vitae deleniti, inventore nulla reprehenderit consequuntur vero impedit non atque
+        voluptates minima odio quas aspernatur minus.
+        Doloremque delectus nesciunt nemo dolores ullam quidem quos doloribus natus blanditiis nobis? Cumque nostrum
+        odit soluta nemo necessitatibus in quidem! Veniam, animi? Laudantium quam commodi ipsa eum voluptatibus
+        architecto a?
+        Amet deserunt voluptate eum, distinctio in voluptates at hic fugiat repudiandae neque veniam libero esse natus
+        rerum nam ad nobis quos ducimus illum numquam quaerat facere et. Magnam, corporis error.
+        Error fugiat accusantium ipsam debitis quaerat! Animi numquam dolores dignissimos ad quidem explicabo ullam ea
+        aperiam aspernatur, illo ipsum tempore laboriosam dicta totam perferendis voluptates rem accusantium aut
+        necessitatibus excepturi.
+        Ut illo sunt accusantium quas fuga eveniet assumenda voluptatem perferendis dicta quam tempore sint inventore
+        error doloribus nisi, eos voluptas ex explicabo rem libero neque? Officiis earum rem quis odit?
+        Porro tenetur omnis praesentium harum tempora minus et illum accusamus saepe, laboriosam iure dignissimos
+        quasi distinctio vitae, nam voluptas officia rerum facere doloremque placeat neque velit doloribus non. Nihil,
+        earum.
+        Itaque sit quam aperiam quaerat distinctio corrupti ut ullam quod minima, voluptates eligendi soluta quis,
+        laboriosam odit labore sint debitis et doloremque natus. Adipisci porro debitis consequuntur deleniti dolorem
+        quisquam.
+        Aliquid temporibus beatae libero iure delectus labore hic enim veniam doloribus earum suscipit perspiciatis
+        nisi, iusto laborum debitis deleniti, cumque ut, iste ex! Maiores numquam laborum quas, pariatur dolor
+        officia?
+        Adipisci rerum mollitia omnis distinctio vero expedita voluptates, consequuntur exercitationem sit esse
+        molestias repellat, ipsum odio dolorem doloribus accusamus impedit quo id porro numquam debitis? Est dolorum
+        consequatur quam fugiat.
+        Deserunt debitis pariatur dolorem, nihil totam esse facilis libero itaque magnam fugit quas fugiat eveniet est
+        aliquid modi voluptas laudantium ab sunt dolore! Quidem quibusdam architecto enim, eveniet distinctio sit.
+        Autem ipsa veniam neque, omnis inventore repudiandae. Consectetur cumque necessitatibus quaerat quo explicabo
+        architecto laboriosam placeat. Esse, harum corporis, dicta eaque nostrum quasi quas repudiandae ipsam qui eos
+        laudantium! Culpa?
+        A ad nihil error nesciunt, veritatis cumque rem consequatur cum eius obcaecati saepe alias magnam, qui quod
+        porro quas veniam commodi. Neque libero accusamus reiciendis, voluptas nisi ratione at debitis.
+        Error explicabo laborum dolorem facere expedita libero doloremque tempora tempore quibusdam repellat nesciunt
+        illo veritatis corporis sequi illum ullam, dolores doloribus obcaecati aliquid! Nobis, necessitatibus minima
+        perferendis sed earum odio.
+        Delectus nostrum atque reprehenderit ex culpa aspernatur ratione praesentium eos repellat quisquam est harum
+        voluptas vero quos eaque, eveniet reiciendis cupiditate, dolor earum provident debitis? Aliquid soluta ipsam
+        delectus vel.
+        Quo in est corporis blanditiis vero recusandae velit fuga quasi, obcaecati nobis quia modi numquam beatae
+        commodi? Incidunt, consequatur eligendi adipisci est minima sapiente hic, officiis suscipit quaerat facilis
+        quos!
+        Cupiditate explicabo dolores provident quos dicta molestiae animi omnis vel in, tempora dolore voluptatum
+        similique maxime nobis delectus id officia expedita repellendus voluptate nihil aliquam at ipsa cumque quam.
+        Recusandae.
+        Numquam, cupiditate, similique expedita, hic aperiam necessitatibus harum est ipsa laboriosam cum unde
+        consequatur a? Nemo molestiae quas expedita voluptatibus quibusdam eum inventore mollitia. Reiciendis soluta
+        id possimus et fugit!
+        Suscipit, natus architecto provident eos enim debitis cupiditate nobis est commodi nemo, non numquam dolores
+        possimus illo? Consequuntur, animi eligendi, quaerat ipsa repellendus minus magni odit, hic quidem illo
+        eveniet?
+        Deleniti laborum a, minima nemo blanditiis qui reiciendis nostrum neque reprehenderit laudantium aspernatur
+        itaque dolorem assumenda iure ad, quo quidem sapiente mollitia veniam commodi, quasi amet! Ut, iusto. Enim,
+        illum.
+        Ratione similique nesciunt tenetur, voluptates dolore doloribus provident cumque possimus facilis. Perferendis
+        est beatae soluta placeat mollitia magnam expedita cumque, accusamus labore minus inventore rem, deserunt
+        nisi, sapiente autem accusantium!
+        A, nihil fugit minus beatae dolorum aut tempora. Quo animi repellendus a, ullam, maiores, inventore
+        necessitatibus dolorem reprehenderit quas ex tenetur porro? Cupiditate unde enim nesciunt assumenda omnis
+        officia vero?
+        Minima obcaecati, ipsa, maxime temporibus quaerat quasi cupiditate odit, dolorem provident itaque optio
+        officia exercitationem praesentium sunt fugit voluptatum rerum laborum distinctio mollitia dolor in minus?
+        Voluptatibus repellat rerum repellendus!
+        Ipsa molestiae vel possimus. Perferendis molestias laudantium vero, animi numquam illo quasi eius ea
+        consectetur consequuntur quis facere ipsa voluptates molestiae, saepe corrupti reiciendis! Inventore veniam
+        earum nulla. Dolorem, deserunt.
+        Debitis amet, eius aliquid distinctio impedit voluptatem qui vel architecto odio optio culpa id rerum itaque
+        iure corporis delectus, saepe quae, suscipit illo ullam deleniti nemo quia nostrum praesentium? Repudiandae.
+        Temporibus, ab similique tempora ea sunt vel facilis sequi dolorem fuga laboriosam corporis fugit voluptatem
+        consectetur quas dignissimos ipsam, assumenda illo omnis tempore quo voluptates modi? Vel itaque voluptatem
+        quod?
+        Soluta nesciunt veritatis unde officia vitae eius excepturi ea eligendi in quod eveniet, numquam placeat
+        corporis non labore enim rem ab nobis, voluptates ad harum nam saepe expedita blanditiis? Doloremque.
+        Voluptate magni reiciendis explicabo et deserunt, totam ad vel eveniet ullam recusandae, enim autem illo qui
+        debitis odit laudantium fugiat pariatur. Voluptatibus dolorum atque autem fugit neque. Beatae, explicabo
+        doloremque.
+        Ipsa cum quisquam maiores amet, reiciendis nisi voluptas distinctio exercitationem similique, laborum dicta
+        voluptates maxime voluptatum dignissimos ratione vero magnam quas odio eius id obcaecati repellat eveniet
+        neque mollitia. Saepe?
+        Possimus recusandae similique eius dolorem quaerat nesciunt harum numquam consequatur incidunt perferendis
+        nemo quasi vel placeat consequuntur, dolores consectetur enim accusantium ad! Accusantium, eos molestiae ipsam
+        minus voluptatibus illum amet?
+        Corporis dolor voluptates soluta, veniam nemo veritatis itaque quis mollitia suscipit omnis officia incidunt
+        repudiandae blanditiis tenetur eveniet iste unde repellendus ex, porro fugit eum ab facilis, iusto ad! Eos!
+        Dignissimos explicabo veritatis nobis aut fugit, possimus iure ab ullam consequatur deserunt animi quo quia
+        non debitis, eum omnis quae? Veniam voluptates eius, vel placeat ut magni repudiandae assumenda odit!
+        Quia quo aliquam eum corrupti, quis eius ratione blanditiis provident architecto dolorem mollitia quas libero
+        nesciunt quibusdam ad eos? Sint reiciendis quae mollitia pariatur placeat tempore molestiae consequuntur qui
+        incidunt!
+        Labore maxime atque exercitationem optio iure eligendi inventore maiores aut vero architecto reiciendis
+        laudantium minus deleniti aperiam, cumque blanditiis suscipit sequi delectus harum aliquam pariatur minima
+        consectetur fugiat. Inventore, quis.
+        Esse porro accusamus aperiam ut est illum repellendus aliquam ea voluptate quibusdam reiciendis accusantium
+        nostrum, dolore consectetur, ipsa animi dignissimos minima nihil consequatur eum earum fugit? Non quasi quod
+        aperiam!</p>
+
+    </div>
+
+    <div className='jugadorContainer'>
+
+      <div className='imagen__jugadorFlex'>
+        <img src={pele} alt="" className='peleIMG' />
+      </div>
+
+      <div className='datosPersonales'>
+        <h4>Datos personales</h4>
+      </div>
+
+      <div className='parrafosPersonales'>
+
+        <p className='parrafoJugador'><span className='spanParrafo'>Nombre completo:</span> Edson Arantes do Nascimiento
+        </p>
+        <p className='parrafoJugador'><span className='spanParrafo'>Apodos:</span> O Rei, La perla negra, D10s</p>
+        <p className='parrafoJugador'><span className='spanParrafo'>Nacimiento:</span> Tres Corações, Minas Gerais,
+          Brasil23 de octubre de 1940. </p>
+        <p className='parrafoJugador'><span className='spanParrafo'>Nacionalidad:</span> Brasileña</p>
+        <p className='parrafoJugador'><span className='spanParrafo'>Fallecimiento:</span> São Paulo, Brasil 29 de
+          diciembre de 2022</p>
+        <p className='parrafoJugador'><span className='spanParrafo'>Altura:</span> 1,73 m 5 8″</p>
+        <p className='parrafoJugador'><span className='spanParrafo'>Debut deportivo:</span> 7 de septiembre de 1956
+          (Santos F. C.)</p>
+        <p className='parrafoJugador'><span className='spanParrafo'>Posición:</span> Delantero</p>
+        <p className='parrafoJugador'><span className='spanParrafo'>Dorsal(es):</span> 10</p>
+        <p className='parrafoJugador'><span className='spanParrafo'>Goles en clubes:</span> 680 (oficiales) 1156 (todos)
+        </p>
+        <p className='parrafoJugador'><span className='spanParrafo'>Retirada deportiva:</span> 1 de octubre de 1977 (New
+          York Cosmos)</p>
+        <p className='parrafoJugador'><span className='spanParrafo'>Selección nacional:</span> Brasil</p>
+        <p className='parrafoJugador'><span className='spanParrafo'>Debut:</span> 7 de julio de 1957</p>
+        <p className='parrafoJugador'><span className='spanParrafo'>Dorsal(es):</span> 10 Part. (goles)92 (77)</p>
 
       </div>
 
     </div>
 
+
   </div>
 
+
 </div>
+
+
+
 )
 }
 
