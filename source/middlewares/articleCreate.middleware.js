a437c4653fc7b0c9db0a09b3a74cbab78908fd33
Julián Viera <julianviera2019@hotmail.com>
2023-02-07 17:44:03 -0300

    Julián - empezando las validaciones desde el back

--- source/middlewares/articleCreate.middleware.js
+++ source/middlewares/articleCreate.middleware.js
@@ -1,5 +1,6 @@
 const multer = require("multer")
 const storage = require("../modules/storage")
 const upload = multer({ storage: storage("/articles") })
+const validations = require("../validations/createArticle")
 
-module.exports = [upload.any()]
+module.exports = [upload.any(), validations]
---
+++ source/validations/createArticle.js
@@ -0,0 +1,7 @@
+const { body } = require("express-validator");
+
+const createProduct = [
+
+]
+
+module.exports = createProduct;
