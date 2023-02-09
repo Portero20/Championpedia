85c7071c81e3d789af4642d7b92904619ad994bb
Julián Viera <julianviera2019@hotmail.com>
2023-02-08 21:13:16 -0300

    Julián - avanzando con las validaciones de futbolistas

--- source/validations/createPlayers.js
+++ source/validations/createPlayers.js
@@ -1,34 +1,91 @@
 const { body } = require("express-validator");
+const { extname, resolve } = require("path")
+const { unlinkSync } = require("fs")
 
 const createPlayers = [
-    // Titulo no vacio, mas de 2 y menos de 100
     body("title").notEmpty().withMessage("El título no debe quedar vacío").bail().isLength({ min: 2 }).withMessage("El título debe contener un mínimo de dos caracteres").bail().isLength({ max: 100 }).withMessage("El título debe contener un máximo de cien caracteres").bail(),
-    // Text no vacio, mas de 100 y menos de 16700000
     body("text").notEmpty().withMessage("Debes completar este campo").bail().isLength({ min: 100 }).withMessage("El texto ingresado es demasiado corto").bail().isLength({ max: 16700000 }).withMessage("El texto es demasiado largo").bail(),
-    // Author no vacio, mas de 2 y menos de 50, no numeros, falta corregir -----------------------------------------------
-    body("author").notEmpty().withMessage("Debes completar tu nombre").bail().isLength({ min: 2 }).withMessage("El nombre es demasiado corto").bail().isLength({ max: 50 }).withMessage("El nombre es demasiado largo").bail().isString().withMessage("No se permiten numeros en el nombre").bail(),
-    // fullName no vacio, mas de 2 y minimo de 100, no numeros, falta corregir ---------------------------------------------
-    body("fullName").notEmpty().withMessage("El nombre no debe quedar vacío").bail().isLength({ min: 2 }).withMessage("El nombre debe tener como mínimo dos caracteres").bail().isLength({ max: 100}).withMessage("El nombre debe tener un máximo de cien caracteres").bail(),
-    // nickName puede quedar vacio, mas de 2 y minimo de 255, hacer custom falta corregir --------------------------------------
-    body("nickName").isLength({ min: 2 }).withMessage("El apodo debe tener como mínimo dos caracteres").bail().isLength({ max: 100}).withMessage("El apodo debe tener un máximo de cien caracteres").bail(),
-    // born no vacio
+    body("author").notEmpty().withMessage("Debes completar tu nombre").bail().isLength({ min: 2 }).withMessage("El nombre es demasiado corto").bail().isLength({ max: 50 }).withMessage("El nombre es demasiado largo").bail(),
+    body("fullName").notEmpty().withMessage("El nombre no debe quedar vacío").bail().isLength({ min: 2 }).withMessage("El nombre debe tener como mínimo dos caracteres").bail().isLength({ max: 100 }).withMessage("El nombre debe tener un máximo de cien caracteres").bail(),
+    body("nickName").isLength({ min: 2 }).withMessage("El apodo debe tener como mínimo dos caracteres").bail().isLength({ max: 100 }).withMessage("El o los apodos deben tener un máximo de cien caracters").bail(),
     body("born").notEmpty().withMessage("La fecha de nacimiento no puede quedar vacía").bail(),
-    // height no vacio, numero, no mas de 9,99 ni 0 o menos
+    body("height").notEmpty().withMessage("La altura no debe quedar vacía").bail().isNumeric().withMessage("La altura debe ser un número").bail().custom(value => {
+        if (value <= 1.50) {
+            throw new Error("La altura debe ser mayor a 1.50")
+        }
 
-    // weight no vacio, numero, no mas de 999,9 ni 0 o menos
+        if (value >= 2.40) {
+            throw new Error("La altura debe ser menor a 2.40")
+        }
 
-    // nationality no vacio, no numero, mas de 2 y menos de 50
+        return true
+    }),
+    body("weight").notEmpty().withMessage("El peso no debe quedar vacío").bail().isNumeric().withMessage("El peso debe ser un número").bail().custom(value => {
+        if (value <= 50) {
+            throw new Error("El peso debe ser mayor a 50Kg")
+        }
 
-    // position no vacia, no numero, mas de 2 y menos de 100
-    body("position").notEmpty().withMessage("La posicion no puede quedar vacia").bail()
+        if (value >= 150) {
+            throw new Error("El peso debe ser menor a 150Kg")
+        }
 
-    // team no vacio, mas de 2 y menos de 100
+        return true
+    }),
+    body("nationality").notEmpty().withMessage("La nacionalidad no debe quedar vacía").bail().isLength({ min: 2 }).withMessage("La nacionalidad debe contener un mínimo de dos caracteres").bail().isLength({ max: 50 }).withMessage("La nacionalidad debe contener un máximo de cincuenta caracteres").bail(),
+    body("position").notEmpty().withMessage("La posicion no puede quedar vacia").bail().isLength({ min: 2 }).withMessage("La posición debe contener un mínimo de dos caracteres").bail().isLength({ max: 100 }).withMessage("La posición debe contener un máximo de cien caracteres").bail(),
+    body("team").notEmpty().withMessage("El equipo no debe quedar vacío").bail().isLength({ min: 2 }).withMessage("El equipo debe contener un mínimo de dos caracteres").bail().isLength({ max: 100 }).withMessage("El equipo debe contener un máximo de cien caracteres").bail(),
+    body("numbers").notEmpty().withMessage("El número no debe quedar vacío").bail().isLength({ max: 100 }).withMessage("El número debe contener un máximo de cien caracteres").bail(),
+    body("goals").notEmpty().withMessage("Los goles no pueden quedar vacíos").bail().custom(value => {
+        if (value < 0) {
+            throw new Error("La cantidad mínima de goles es de 0")
+        }
 
-    // numbers no vacio, mas de 2 y menos de 100
+        if (value > 1400) {
+            throw new Error("La cantidad máxima de goles es de 1400")
+        }
 
-    // goals no vacio, mas de 0 y menos de 5000
+        return true
+    }),
+    body("debut").notEmpty().withMessage("La fecha de debut no puede quedar vacía").bail(),
+    body("image").custom((value, { req }) => {
+        let imagen = req.files
 
-    // debut no vacio
+        if (!imagen || imagen.length == 0) {
+            throw new Error("La imagen no puede quedar vacía")
+        }
+
+        let extensiones = [".svg", ".jpg", ".png", ".jpeg"]
+        let extension = extname(imagen[0].filename)
+        if (!extensiones.includes(extension)) {
+            unlinkSync(resolve(__dirname, "../../uploads/articles/" + imagen[0].filename))
+            throw new Error("La extension debería ser '.svg', '.jpg', '.png', '.jpeg'")
+        }
+
+        if (imagen[0].size > 2097152) {
+            unlinkSync(resolve(__dirname, "../../uploads/articles/" + imagen[0].filename))
+            throw new Error("La imagen supera el peso de 2MB");
+        }
+
+        if (req.files.length > 1) {
+            req.files.forEach(img => {
+                unlinkSync(resolve(__dirname, "../../uploads/articles/" + img.filename))
+            });
+            throw new Error("No puedes subir más de una imagen");
+        }
+
+        return true
+    }),
+    body("tags").notEmpty().withMessage("Los tags no deben quedar vacíos").bail().custom(value => {
+        tags = value.split(",").map(tag => {
+            return tag
+        })
+
+        if (tags.length > 10) {
+            throw new Error("Puedes agregar un máximo de diez tags")
+        }
+        
+        return true
+    })
 
 ]
 
