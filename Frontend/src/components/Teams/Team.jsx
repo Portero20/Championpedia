76ce91fcb653773dcb665d6dcb5f51d1c10365ab
Julián Viera <julianviera2019@hotmail.com>
2023-03-06 19:09:58 -0300

    Julián - vista editar equipos terminada, falta alta tgs y mejora de código

--- Frontend/src/components/Teams/Team.jsx
+++ Frontend/src/components/Teams/Team.jsx
@@ -3,7 +3,15 @@ import '../../scss/utilities/_utilities.scss';
 
 import React from 'react'
 
-const Team = ({article}) => {
+const Team = ({ showValue, article }) => {
+    let InputAuthor;
+
+    if (showValue != true) {
+
+        InputAuthor = <div className='inputs-second'><label>Autor:</label><input type="text" name="author" id="author" placeholder='Nombre del autor de este 
+      artículo'className='inputs__team' />
+            <p className="msg-error"></p></div>
+    }
     return (
         <div>
             <div className='containerEquipos'>
@@ -11,40 +19,38 @@ const Team = ({article}) => {
                 <div className='flexTeam'>
 
                     <label>Título:</label>
-                    <input type="text" name="title" id="title" placeholder='Título del artículo...' className='inputs__team'/>
+                    <input type="text" name="title" id="title" placeholder='Título del artículo...' className='inputs__team' value={showValue ? article.title : null}/>
                     <p className="msg-error"></p>
 
                     <label>Nombre completo:</label>
-                    <input type="text" name="fullName" id="fullName" placeholder='Nombre completo del equipo...' className='inputs__team' />
+                    <input type="text" name="fullName" id="fullName" placeholder='Nombre completo del equipo...' className='inputs__team' value={showValue ? article.fullName : null}/>
                     <p className="msg-error"></p>
 
                     <label>Apodos: (Opcional)</label>
-                    <input type="text" name="nickName" id="nickName" placeholder='Apodo(s) del equipo...' className='inputs__team' />
+                    <input type="text" name="nickName" id="nickName" placeholder='Apodo(s) del equipo...' className='inputs__team' value={showValue ? article.nickName : null}/>
                     <p className="msg-error"></p>
 
                     <label>Fundación:</label>
-                    <input type="date" name="foundation" id="foundation" className='inputs__team' />
+                    <input type="date" name="foundation" id="foundation" className='inputs__team' value={showValue ? article.foundation.split("T")[0] : null}/>
                     <p className="msg-error"></p>
 
                     <label>Presidente:</label>
-                    <input type="text" name="president" id="president" placeholder='Nombre del presidente del equipo...' className='inputs__team' />
+                    <input type="text" name="president" id="president" placeholder='Nombre del presidente del equipo...' className='inputs__team' value={showValue ? article.president : null}/>
                     <p className="msg-error"></p>
 
                     <label>Estadio:</label>
-                    <input type="text" name="stadium" id="stadium" placeholder='Nombre del estadio del equipo...' className='inputs__team' />
+                    <input type="text" name="stadium" id="stadium" placeholder='Nombre del estadio del equipo...' className='inputs__team' value={showValue ? article.stadium : null}/>
                     <p className="msg-error"></p>
 
                     <label>Entrenador:</label>
-                    <input type="text" name="coach" id="coach" placeholder='Nombre del entrenador del equipo...' className='inputs__team' />
+                    <input type="text" name="coach" id="coach" placeholder='Nombre del entrenador del equipo...' className='inputs__team' value={showValue ? article.coach : null}/>
                     <p className="msg-error"></p>
 
                     <label>Imagen:</label>
                     <input type="file" name='file' id="file" className='inputFile3' />
                     <p className="msg-error"></p>
 
-                    <label>Autor:</label>
-                    <input type="text" name="author" id="author" placeholder='Nombre del autor de este artículo' className='inputs__team' />
-                    <p className="msg-error"></p>
+                    {InputAuthor}
 
                 </div>
 
