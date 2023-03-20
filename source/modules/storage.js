d9c2baa0187f72346c7a21929f81945337b97fdb
Julián Viera <julianviera2019@hotmail.com>
2023-03-20 20:42:44 -0300

    Julián - probando git pull

--- source/modules/storage.js
+++ source/modules/storage.js
@@ -1,16 +1,7 @@
-const { diskStorage } = require("multer")
-const { extname } = require("path")
+// Importar multer
+const multer = require('multer');
 
-let destination = folder => (req, file, cb) => cb(null, "./uploads/" + folder)
-
-let filename = (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix + extname(file.originalname))
-}
-
-const storage = folder => diskStorage({
-    destination: destination(folder),
-    filename: filename
-})
+// Configuración de multer
+const storage = multer.memoryStorage();
 
 module.exports = storage
