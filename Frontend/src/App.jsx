99be78d35db8682c26674d2b46a46539eca5d3a4
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-04-21 12:34:06 -0300

    Exequiel - Agregando condicionales para el loader

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,16 +4,39 @@ import Footer from "./components/Footer/Footer";
 import Links from "./common/Links/Links";
 import Main from "./components/Main/Main";
 import Navbar from "./components/Navbar/Navbar";
+import Loader from './components/Loader/Loader';
+import { useState } from 'react';
 
 const App = () => {
+
+  const [isLoading,setIsLoading] = useState(true);
+
+
+  setTimeout(() => {
+
+    setIsLoading(false)
+    
+  }, 3000);
 
   return (
     
     <>
-    
-      <Navbar/>
-      <Main/>
-      <Footer/>
+
+      {isLoading ? (
+
+        <Loader />
+
+      ) : (
+
+        <div className='content'>
+
+          <Navbar />
+          <Main />
+          <Footer />
+          
+        </div>
+
+      )}
 
     </>
     
