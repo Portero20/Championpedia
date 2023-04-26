fcf439ee377fe799df0369c82f07d83551f347b0
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-04-25 21:45:29 -0300

    Exequiel - Corrigiendo un poco el footer

--- Frontend/src/components/Footer/Footer.jsx
+++ Frontend/src/components/Footer/Footer.jsx
@@ -11,75 +11,75 @@ import { SiWhatsapp } from 'react-icons/si'
 const Footer = () => {
 
     return (
-       <>
-       
-        <div className="containerFooter">
+        <>
 
-            <div className="containerFooter__logo">
+            <div className="containerFooter">
 
-                <div className='hijoLogo'>
+                <div className="containerFooter__logo">
 
-                    <div className='containerEnlaces'>
+                    <div className='hijoLogo'>
 
-                        <Link to="/">
-                            <img src={Championpedia} alt="Championpedia" className='logoChampion' />
-                        </Link>
+                        <div className='containerEnlaces'>
 
-                        <div className='enlaceHijo'>
+                            <Link to="/">
+                                <img src={Championpedia} alt="Championpedia" className='logoChampion' />
+                            </Link>
 
-                            <Link to="/" className='enlace'>Inicio</Link>
+                            <div className='enlaceHijo'>
+
+                                <Link to="/" className='enlace'>Inicio</Link>
+
+                            </div>
+
+                            <div className='enlaceHijo'>
+
+                                <Link to='/' className='enlace'>Nosotros</Link>
+
+                            </div>
+
+                            <div className='enlaceHijo'>
+
+                                <Link to="/category/copas" className='enlace'>Copas</Link>
+
+                            </div>
+
+                            <div className='enlaceHijo'>
+
+                                <Link to="/category/equipos" className='enlace'>Equipos</Link>
+
+                            </div>
+
+                            <div className='enlaceHijo'>
+
+                                <Link to="/category/jugadores" className='enlace'>Futbolistas</Link>
+
+                            </div>
 
                         </div>
+                    </div>
 
-                        <div className='enlaceHijo'>
+                    <div className="buscadorContainer">
 
-                            <Link to="#" className='enlace'>Nosotros</Link>
+                        <div className="socialContainer">
 
-                        </div>
+                            <div className="hijoSocial">
 
-                        <div className='enlaceHijo'>
+                                <Link to="#" className='enlaceSocial'><AiFillTwitterCircle className='iconSocial' color='white' /></Link>
+                                <Link to="#" className='enlaceSocial'><RxInstagramLogo className='iconSocial' color='white' /></Link>
+                                <Link to="#" className='enlaceSocial'><SiWhatsapp className='iconSocial' color='white' /></Link>
 
-                            <Link to="/category/copas" className='enlace'>Copas</Link>
-
-                        </div>
-
-                        <div className='enlaceHijo'>
-
-                            <Link to="/category/equipos" className='enlace'>Equipos</Link>
-
-                        </div>
-
-                        <div className='enlaceHijo'>
-
-                            <Link to="/category/jugadores" className='enlace'>Futbolistas</Link>
+                            </div>
 
                         </div>
 
                     </div>
-                </div>
-
-                <div className="buscadorContainer">
-
-                    <div className="socialContainer">
-
-                        <div className="hijoSocial">
-
-                            <Link to="#" className='enlaceSocial'><AiFillTwitterCircle className='iconSocial' color='white' /></Link>
-                            <Link to="#" className='enlaceSocial'><RxInstagramLogo className='iconSocial' color='white' /></Link>
-                            <Link to="#" className='enlaceSocial'><SiWhatsapp className='iconSocial' color='white' /></Link>
-
-                        </div>
-
-                    </div>
 
                 </div>
+
 
             </div>
 
-
-        </div>
-
-       </>
+        </>
     );
 }
 
