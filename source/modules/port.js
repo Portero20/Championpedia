941e4556753f31b23b3b3d6f19690488da62c13d
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-05-04 17:36:39 -0300

    Exequiel - Modificando el puerto

--- source/modules/port.js
+++ source/modules/port.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv').config();
 
 module.exports = {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 10000,
     start: () => console.log(`Server running on port ${process.env.PORT}`)
 }
