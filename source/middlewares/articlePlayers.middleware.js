fd0ded99204ddda2f0c1150046ddc7f18af56d86
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-05-04 21:04:37 -0300

    Exequiel - Dividiendo en carpetas

--- source/middlewares/articlePlayers.middleware.js
+++
@@ -1,8 +0,0 @@
-const multer = require("multer")
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-const validations = require("../validations/createPlayers")
-
-const { verificarToken } = require("../middlewares/verifyToken.middleware")
-
-module.exports = [verificarToken, upload.array("file", 1), validations]
--- source/routes/database.routes.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-const dbControllers = require("../controllers/database.controllers")
-
-router.get("/create", dbControllers.create);
-
-module.exports = router
