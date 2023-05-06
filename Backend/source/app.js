88d350c92e27ea7f8828ba1875b16c3b1554bf5d
Julián Viera <julianviera2019@hotmail.com>
2023-05-06 00:03:37 -0300

    Julián - Probando

--- Backend/source/app.js
+++ Backend/source/app.js
@@ -1,21 +1,21 @@
 const express = require('express')
 const app = express()
+const cors = require("cors")
 const { port, start } = require("./modules/port")
 const bodyParser = require('body-parser');
-const cors = require("cors")
 
 app.get('/', (req, res) => {
     res.send("Hello World!")
 })
 
-app.listen(port, start)
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(cors());
-
-app.use("/database", require("./routes/database.routes"));
+app.use("/token", require("./routes/token.routes"))
 app.use("/article", require("./routes/articles.routes"));
 app.use("/search", require("./routes/search.routes"));
-app.use("/token", require("./routes/token.routes"))
+app.use("/database", require("./routes/database.routes"));
+
+app.listen(port, start)
