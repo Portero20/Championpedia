4cdcc4c06a28f68260bafd88925887711577ca3f
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-05-05 22:57:08 -0300

    Exequiel - Cambios en el backend

--- Backend/source/app.js
+++ Backend/source/app.js
@@ -4,18 +4,20 @@ const { port, start } = require("./modules/port")
 const bodyParser = require('body-parser');
 const cors = require("cors")
 
-app.get('/', (req, res) => {
-    res.send("Hello World!")
-})
+app.options('*', cors())
 
-app.listen(port, start)
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cors());
+app.get('/', (req, res) => {
+    res.send("Hello World!")
+    
+})
 
 app.use("/database", require("./routes/database.routes"));
 app.use("/article", require("./routes/articles.routes"));
 app.use("/search", require("./routes/search.routes"));
 app.use("/token", require("./routes/token.routes"))
+app.listen(port, start)
