942ad0389c8e8dcf6220d41b27b724571e32bc1e
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-05-05 23:27:48 -0300

    Exequiel - Actualizando backend

--- Backend/source/app.js
+++ Backend/source/app.js
@@ -4,20 +4,18 @@ const { port, start } = require("./modules/port")
 const bodyParser = require('body-parser');
 const cors = require("cors")
 
-app.options('*', cors())
+app.get('/', (req, res) => {
+    res.send("Hello World!")
+})
 
+app.listen(port, start)
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cors());
-app.get('/', (req, res) => {
-    res.send("Hello World!")
-    
-})
 
 app.use("/database", require("./routes/database.routes"));
 app.use("/article", require("./routes/articles.routes"));
 app.use("/search", require("./routes/search.routes"));
 app.use("/token", require("./routes/token.routes"))
-app.listen(port, start)
