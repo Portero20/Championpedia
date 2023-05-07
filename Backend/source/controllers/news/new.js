2265cd42556be62846de63c5f22f8d728b7f139e
Julián Viera <julianviera2019@hotmail.com>
2023-05-07 00:19:06 -0300

    Julián - Arreglos api news

--- Backend/source/controllers/news/new.js
+++ Backend/source/controllers/news/new.js
@@ -5,7 +5,7 @@ module.exports = {
         try {
             const now = moment().format("YYYY/MM/DD")
 
-            const url = `https://newsapi.org/v2/everything?language=es&q=futbol&to=07/04/2023&sortBy=publishedAt&apiKey=ec7319b16ecc4bd0bf705eb7bcbad8f0`;
+            const url = `https://newsapi.org/v2/everything?language=es&q=futbol&to=${now}&sortBy=publishedAt&apiKey=ec7319b16ecc4bd0bf705eb7bcbad8f0`;
 
             const response = await fetch(url);
             const data = await response.json();
@@ -17,7 +17,11 @@ module.exports = {
                     url: article.url,
                     published: article.publishedAt
                 }
-            }).slice(0, 3);
+            })
+
+            if (news.length > 0) {
+                news.slice(0, 3);
+            }
 
             return res.status(200).json(news);
         } catch (error) {
