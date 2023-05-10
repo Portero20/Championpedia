b27893a57a0918aceb2ac486af0430e15ca78d43
Julián Viera <julianviera2019@hotmail.com>
2023-05-10 19:59:00 -0300

    Julián - Arreglos en el create

--- Backend/source/controllers/articles/edit.js
+++ Backend/source/controllers/articles/edit.js
@@ -34,6 +34,8 @@ module.exports = {
             let nickName = req.body.nickName ? req.body.nickName.replace(/"/g, '\\"') : "";
             let death = req.body.death
             let retire = req.body.retire
+            console.log(death, "|", retire);
+            console.lop(retire != null ? `"${retire}"` : null)
 
             if (category == "players") {
                 query = `UPDATE players SET title="${title}",text="${text}",fullName="${fullName}",nickName="${nickName}",born="${req.body.born}", death=${death != null ? `"${death}"` : null}, height="${req.body.height}",weight="${req.body.weight}",nationality="${req.body.nationality}",position="${req.body.position}",team="${req.body.team}",numbers="${req.body.numbers}",goals="${req.body.goals}",debut="${req.body.debut}", retire=${retire != null ? `"${retire}"` : null} WHERE id = ${req.body.id};`
