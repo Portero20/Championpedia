1570a7adde45460f4192508b3e5df76804d8793b
Exequiel Portero <95886541+Portero20@users.noreply.github.com>
2023-05-11 19:03:14 -0300

    Exequiel - Creando JSON de noticias

---
+++ Frontend/src/json/noticias.js
@@ -0,0 +1,45 @@
+export const NEWS = {
+
+    "articles": [{
+        "source": {
+            "id": "la-nacion",
+            "name": "La Nacion"
+        },
+        "author": "LA NACION",
+        "title": "Martín Demichelis le respondió a Navarro Montoya: “No estamos para competirle al City”",
+        "description": "El entrenador de River Plate pidió mesura en relación a los halagos que recibió su equipo, aunque admitió que va por la buena senda",
+        "url": "https://www.lanacion.com.ar/deportes/martin-demichelis-le-respondio-a-navarro-montoya-no-estamos-para-competirle-al-city-nid10052023/",
+        "urlToImage": "https://resizer.glanacion.com/resizer/c4oHGiUQk05fe-Pdr0lRrgXnTug=/768x0/filters:format(webp):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/FVQOR7XQA5AHNOBLLQWEEWC7BI.jpg",
+        "publishedAt": "2023-05-10T21:22:35Z",
+        "content": "El entrenador de River Plate, Martín Demichelis, salió al cruce de las declaraciones que hizo semanas atrás Navarro Montoya, columnista de F90 (ESPN), y sentenció que su equipo no está en condiciones… [+2967 chars]"
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "El Financiero"
+        },
+        "author": "Redacción",
+        "title": "Milan vs. Inter: Minuto a minuto en vivo de la semifinal de ida de la Champions League",
+        "description": "AC Milan e Inter se enfrentan en el estadio San Siro en la ida de semifinales de la Champions League. Sigue aquí el minuto a minuto en vivo de las acciones más importantes y videos de los goles.",
+        "url": "https://www.elfinanciero.com.mx/deportes/2023/05/10/champions-league-2023-semifinal-milan-vs-inter-minuto-a-minuto-en-vivo-del-partido-y-goles/",
+        "urlToImage": "https://www.elfinanciero.com.mx/resizer/X-Iu8-dR9AuaD-2mGT-Ai3qUVMQ=/1200x630/filters:format(jpg):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/elfinanciero/H4MYJZKXWREC7NAPLK6ER3HDGE.jpg",
+        "publishedAt": "2023-05-10T19:51:32Z",
+        "content": "Solo hay una ciudad en toda Europa que puede presumir de tener dos equipos que se enfrentarán este miércoles en el primer capítulo de una apasionante semifinal por ver no solo quien accede a la final… [+2749 chars]"
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "Aristeguinoticias.com"
+        },
+        "author": "Redacción AN / JGE",
+        "title": "Fluminense, Cruzeiro y Paranaense separan a futbolistas investigados por amaños en Brasil",
+        "description": "Un tribunal del estado de Goiás aceptó este martes la denuncia de la Fiscalía contra 16 jugadores por participar en la manipulación de los resultados.",
+        "url": "https://aristeguinoticias.com/1005/deportes/fluminense-cruzeiro-y-paranaense-separan-a-futbolistas-investigados-por-amanos-en-brasil/",
+        "urlToImage": "https://editorial.aristeguinoticias.com/wp-content/uploads/2023/05/brasil-amano-partidos-fluminense-10052023.jpg",
+        "publishedAt": "2023-05-10T19:36:03Z",
+        "content": "Fluminense, Cruzeiro y Athletico Paranaense, clubes de la primera división del futbol de Brasil, separaron provisionalmente este miércoles a jugadores investigados por presuntamente haber recibido so… [+3047 chars]"
+    },
+
+    ]
+
+}
